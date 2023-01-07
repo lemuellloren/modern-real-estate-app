ce376a698178f9840774f4b3b7584fe51e360a26
Lemuel Lloren <lemuelpaconlloren@gmail.com>
2023-01-07 16:29:31 +0800

    Setup footer component

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/layout";
+
+const year = new Date().getFullYear();
+
+const Footer = () => (
+  <Box textAlign='center' p='5' color='gray.600' borderTop='1px' borderColor='gray.100'>
+    &copy; {year} Realtor, Inc.
+  </Box>
+);
+
+export default Footer;
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,8 @@
 import Head from "next/head";
 import { Box } from "@chakra-ui/react";
 
-// import Footer from "./Footer";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const Layout = ({ children }) => {
   return (
@@ -15,7 +15,9 @@ const Layout = ({ children }) => {
           <Navbar />
         </header>
         <main>{children}</main>
-        <footer>{/* <Footer /> */}</footer>
+        <footer>
+          <Footer />
+        </footer>
       </Box>
     </>
   );
