b288418f53b2b903c0160b6025b18b17932d269b
Lemuel Lloren <lemuelpaconlloren@gmail.com>
2023-01-07 20:04:14 +0800

    Update header key

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,7 +5,7 @@ export const baseUrl = "https://bayut.p.rapidapi.com";
 export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
-      "X-RapidAPI-Key": "35c80719bamsh88d9f04d718690cp1b954ajsn1b41755ec19a",
+      "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY ,
       "X-RapidAPI-Host": "bayut.p.rapidapi.com",
     },
   });
