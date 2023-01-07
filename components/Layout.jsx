d42b57b12ae51614f99112679ebe32ede4626712
Lemuel Lloren <lemuelpaconlloren@gmail.com>
2023-01-07 16:25:10 +0800

    Add navbar component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,19 +2,23 @@ import Head from "next/head";
 import { Box } from "@chakra-ui/react";
 
 // import Footer from "./Footer";
-// import Navbar from "./Navbar";
+import Navbar from "./Navbar";
 
-export default function Layout({ children }) {
+const Layout = ({ children }) => {
   return (
     <>
       <Head>
         <title>Realtor</title>
       </Head>
       <Box maxWidth="1280px" m="auto">
-        <header>{/* <Navbar /> */}</header>
+        <header>
+          <Navbar />
+        </header>
         <main>{children}</main>
         <footer>{/* <Footer /> */}</footer>
       </Box>
     </>
   );
-}
+};
+
+export default Layout;
